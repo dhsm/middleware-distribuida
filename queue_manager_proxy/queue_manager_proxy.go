ff95ce7222803f28d43e473c23b281aaec2da0b6
Déborah Mesquita <deh.mesquita@hotmail.com>
2016-11-19 23:32:19 -0300

    Creating method send of QueueManagerProxy

--- queue_manager_proxy/queue_manager_proxy.go
+++ queue_manager_proxy/queue_manager_proxy.go
@@ -1,9 +1,15 @@
 package queue_manager_proxy
 
+import . "../client_request_handler"
+
 type QueueManagerProxy struct {
   Queue_name string
 }
 
-func (qmp *QueueManagerProxy) send(m string){
-
+func (qmp *QueueManagerProxy) send(msg Message){
+  crh := ClientRequestHandler{}
+  crh.NewCRH("tcp", "127.0.0.1:8081")
+  packet := Packet{qmp.Queue_name, msg}
+  packetMarshaled, _ := json.Marshal(packet)
+  crh.send(packetMarshaled)
 }
