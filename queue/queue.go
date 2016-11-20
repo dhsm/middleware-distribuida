cb0f96a85d02399877d61b542fde5bdf90828a22
Bruno Soares da Silva <brunosilvapaz@gmail.com>
2016-11-20 01:32:21 +0000

    cleaning to merge

--- queue/queue.go
+++ queue/queue.go
@@ -4,31 +4,3 @@ import . "../message"
 import "fmt"
 import "container/heap"
 
-type PriorityQueue []*Message
-
-func (pq PriorityQueue) Len() int {
-	return len(pq)
-}
-
-func (pq PriorityQueue) Less(i, j int) bool {
-	return pq[i].Priority < pq[j].Priority
-}
-
-func (pq PriorityQueue) Swap(i, j int){
-    pq[i], pq[j] = pq[j], pq[i]
-}
-
-func (pq PriorityQueue) Push(x interface{}){
-	item := x.(*Message)
-	pq = append(pq, item)
-	heap.Fix(pq, len(pq))
-	fmt.Println("Um push foi realizado", len(pq))
-}
-
-func (pq PriorityQueue) Pop() interface{}{
-	old := pq
-	n := len(old)
-	item := old[n-1]
-	pq = old[0: n-1]
-	return item
-}
