fb56283914ad09315226471bf3ccd4d74d56504c
Déborah Mesquita <deh.mesquita@hotmail.com>
2016-11-19 23:40:05 -0300

    Constructor for QueueManagerProxy

--- queue_manager_proxy/queue_manager_proxy.go
+++ queue_manager_proxy/queue_manager_proxy.go
@@ -6,6 +6,10 @@ type QueueManagerProxy struct {
   Queue_name string
 }
 
+func (qmp *QueueManagerProxy) NewQMProxy(queue_name string,){
+  qmp.Queue_name = queue_name
+}
+
 func (qmp *QueueManagerProxy) send(msg Message){
   crh := ClientRequestHandler{}
   crh.NewCRH("tcp", "127.0.0.1:8081")
