0695c2bc60b7b66bb9c3bb2032a4559cd8aa95b4
Bruno Soares da Silva <brunosilvapaz@gmail.com>
2016-11-20 00:18:26 +0000

    package queue ok

--- queue/queue.go
+++ queue/queue.go
@@ -2,7 +2,6 @@ package queue
 
 import . "../message"
 import "container/heap"
-import "fmt"
 
 type PriorityQueue []*Message
 
@@ -15,65 +14,32 @@ func (pq PriorityQueue) Less(i, j int) bool {
 	return pq[i].Priority < pq[j].Priority
 }
 
-func (q PriorityQueue) Push(x interface{}){
-	n := len(*q)
+func (pq *PriorityQueue) Push(x interface{}){
+	n := len(*pq)
 	item := x.(*Message)
-	item.index = n
-	*q = append(*q, item)
+	item.Index = n
+	*pq = append(*pq, item)
 }
 
 func (pq *PriorityQueue) Pop() interface{}{
 	old := *pq
 	n := len(old)
 	item := old[n-1]
-	item.index = -1
+	item.Index = -1
 	*pq = old[0: n-1]
 	return item
 }
 
 func (pq *PriorityQueue) update(msg *Message, msgtext string, priority int){
-    msg.msgtext = msgtext
-    msg.priority = priority
-    heap.Fix(pq, msg.index)
+    msg.Msgtext = msgtext
+    msg.Priority = priority
+    heap.Fix(pq, msg.Index)
 }
 
 func (pq PriorityQueue) Swap(i, j int){
     pq[i], pq[j] = pq[j], pq[i]
-    pq[i].index = i
-    pq[j].index = j
+    pq[i].Index = i
+    pq[j].Index = j
 }
 
-func main(){
-	// Some items and their priorities.
-	items := map[string]int{
-		"Banana is yellow": -1, "Apple is rich": 1, "Pear is for tea": 4,
-	}
 
-	// Create a priority queue, put the items in it, and
-	// establish the priority queue (heap) invariants.
-	pq := make(PriorityQueue, len(items))
-	i := 0
-	for value, priority := range items {
-		pq[i] = &Item{
-			value:    value,
-			priority: priority,
-			index:    i,
-		}
-		i++
-	}
-	heap.Init(&pq)
-
-	// Insert a new item and then modify its priority.
-	item := &Item{
-		value:    "orange",
-		priority: 1,
-	}
-	heap.Push(&pq, item)
-	pq.update(item, item.value, 5)
-
-	// Take the items out; they arrive in decreasing priority order.
-	for pq.Len() > 0 {
-		item := heap.Pop(&pq).(*Item)
-		fmt.Printf("%.2d:%s ", item.priority, item.value)
-	}
-}
