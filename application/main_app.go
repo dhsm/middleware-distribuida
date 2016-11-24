210e0acf456aae5b4ed873ded9867a01b2ef3531
Déborah Mesquita <deh.mesquita@hotmail.com>
2016-11-24 02:37:30 -0300

    Rewrite topic_manager

--- application/main_app.go
+++ application/main_app.go
@@ -12,9 +12,14 @@ func main() {
   topic := session.CreateTopic("arborismo")
 
   publisher := session.CreateTopicPublisher(topic)
+  subscriber1 := session.CreateTopicSubscriber(topic)
+  subscriber2 := session.CreateTopicSubscriber(topic)
+  subscriber3 := session.CreateTopicSubscriber(topic)
 
   conn.Start()
-  
+
+  subscriber1.SetMessageListener()
+
   publisher.Send(session.CreateMessage("Pau que nasce torto", "arborismo",1,"m1"))
   publisher.Send(session.CreateMessage("Nunca de endireita","arborismo", 5,"m2"))
 }
--- topic_manager/topic_manager.go
+++ topic_manager/topic_manager.go
@@ -4,44 +4,41 @@ import . "../message"
 import . "../topic"
 
 type TopicManager struct {
-  ActiveTopics map[string]*Topic
+  ActiveTopics map[string]int
+  Root Node
 }
 
-func (tpcManager *TopicManager) addMessageToTopic(topicname string, msg Message){
-  if(! topicExists(topicname, tpcManager.ActiveTopics)){
-    createTopic(topicname)
-  }
-  topic := getTopic(topicname, tpcManager.ActiveTopics)
-  tpcManager.ActiveTopics[string] = topic
-  topic.AddMessage(msg)
+func (tpcManager *TopicManager) CreateTopicManager(){
+  tpcManager.ActiveTopics = make(map[string]int)
 }
 
-func (tpcManager *TopicManager) Subscribe(topicname string, clientId string){
-  if(! topicExists(topicname, tpcManager.ActiveTopics)){
-    createTopic(topicname)
-  }
-  topic := getTopic(topicname, tpcManager.ActiveTopics)
-  topic.AddSubscriber(clientId)
+func (tpcManager *TopicManager) Create(topic_name string){
+  node := Node{}
+  Node.CreateNode(topic_name)
+}
+
+func (tpcManager *TopicManager) AddMessageToTopic(topicname string, msg Message){
+  tpcManager.ActiveTopics[topic_name] = 1
+  msgs := tpcManager.Rode.GetMessages()
+  heap.Init(&msgs)
+  heap.Push(&msgs, &msg)
+  tpcManager.Rode.SetMessages(msgs)
+  tpcManager.Root.SetTotalMessages(tpcManager.Root.GetTotalMessages() + 1)
+}
+
+func (tpcManager *TopicManager) Subscribe(topic_name string, clientId string){
+  subscribers := tpcManager.Root.GetSubscribed()
+  subscribers[topic_name] = 1
+  tpcManager.Root.SetSubscribed() = subscribers
 }
 
 func (tpcManager *TopicManager) Unsubscribe(topicname string, clientId string){
-  if(topicExists(topicname, tpcManager.ActiveTopics)){
-    topic := getTopic(topicname,, tpcManager.ActiveTopics)
-    topic.RemoveSubscriber(clientId)
+  subscribers := tpcManager.Root.GetSubscribed()
+  for i, v := range subscribers {
+    if(v == clientId ){
+      copy(subscribers[i:], subscribers[i+1:])
+      subscribers = subscribers[:len(subscribers)-1]
+    }
   }
-}
-
-func topicExists(topicname string, activeTopics map[string]*Topic) bool{
-  val, ok := activeTopics[topicname]
-  return ok
-}
-
-func createTopic(topicname string){
-  topic := Topic{}
-  topic.CreateTopic(topicname)
-}
-
-func getTopic(topicname string, activeTopics map[string]*Topic){
-  val, ok := activeTopics[topicname]
-  return val
+  tpcManager.Root.SetSubscribed() = subscribers
 }
